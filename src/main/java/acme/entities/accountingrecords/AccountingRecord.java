cbda062e285c35048136926341d19ae05d4a29de
viclopvaz1 <vlopezvazquez3@gmail.com>
2020-08-18 13:20:33 +0200

    Añadidos Indices

--- src/main/java/acme/entities/accountingrecords/AccountingRecord.java
+++ src/main/java/acme/entities/accountingrecords/AccountingRecord.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -22,6 +24,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "status")
+})
 public class AccountingRecord extends DomainEntity {
 
 	public static final long	serialVersionUID	= 1L;
