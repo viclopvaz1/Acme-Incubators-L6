512e76b7cc6292c5f4b0835873a21ef277d76031
viclopvaz1 <vlopezvazquez3@gmail.com>
2020-08-23 16:33:54 +0200

    Arreglos

--- src/main/java/acme/entities/technologyrecords/TechnologyRecord.java
+++ src/main/java/acme/entities/technologyrecords/TechnologyRecord.java
@@ -3,6 +3,8 @@ package acme.entities.technologyrecords;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -16,6 +18,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "sector, indication")
+})
 public class TechnologyRecord extends DomainEntity {
 
 	public static final long	serialVersionUID	= 1L;
